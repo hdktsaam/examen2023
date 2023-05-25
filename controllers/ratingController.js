f498da2d6acd90c5a5eb6d5e6972d12f859fac50
Hannes De Kesel <hannes.dekesel@tsaam.be>
2023-05-25 10:06:57 +0200

    change result rating

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -3,7 +3,7 @@ const db = require("../databank");
 const getRating = (req, res) => {
   db.all("select * from rating", [], (err, rows) => {
     if (err) return res.json({ message: err.message });
-    res.json({ user_id: req.params.id, lessen: rows });
+    res.json({ ratings: rows });
   });
 };
 
@@ -13,7 +13,7 @@ const getRatingByCourse = (req, res) => {
     [req.params.id],
     (err, rows) => {
       if (err) return res.json({ message: err.message });
-      res.json({ user_id: req.params.id, lessen: rows });
+      res.json({ ratings: rows });
     }
   );
 };
@@ -25,7 +25,7 @@ const addRating = (req, res) => {
     aantalSterren: req.body.aantal,
   };
   db.run(
-    "insert into rating (user_id, lessen_id,AantalSterren) values (?,?,?)",
+    "insert into rating (user_id, lessen_id, AantalSterren) values (?,?,?)",
     [newRating.user_id, newRating.les_id, newRating.aantalSterren],
     (err) => {
       if (err) return res.json({ message: err.message });
